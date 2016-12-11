59f0218e34edd9a934e8c732f0881e82186f04a7
The Blink Man <cardosoalanf@gmail.com>
2016-12-11 16:53:21 -0200

    Add function to make sockets reusable

--- src/main.c
+++ src/main.c
@@ -1,8 +1,34 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+
+void make_socket_reusable(int listen_sfd);
 
 int main(int argc, char *argv[])
 {
+  int listen_sfd;
+  listen_sfd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
+  make_socket_reusable(listen_sfd);
   
   return 0;
+}
+
+
+/*!
+ * \brief Modifica as opcoes da socket para ela ser reusavel
+ */
+void make_socket_reusable(int listen_sfd)
+{
+  int status;
+  int on = 1;
+
+  status = setsockopt(listen_sfd, SOL_SOCKET, SO_REUSEADDR,
+		      (char *)&on, sizeof(on));
+  if (status < 0)
+  {
+    perror("setsockopt() failed");
+    close(listen_sfd);
+    exit(-1);
+  }
 }
