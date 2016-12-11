b237c86829f40276069fb84f4ae1f06d6d333df3
The Blink Man <cardosoalanf@gmail.com>
2016-12-11 20:27:44 -0200

    Add poll and the structures it uses to the server and control the flow of the server by the returned events of the poll call

--- src/main.c
+++ src/main.c
@@ -1,47 +1,149 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<arpa/inet.h>
-#include<unistd.h>
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <signal.h>
+#include <poll.h>
+#include <errno.h>
 
-//#define HOSTADDR "127.0.0.1"
-#define HOSTADDR "192.168.25.12"
+#define HOSTADDR "127.0.0.1"
+//#define HOSTADDR "192.168.25.12"
 #define MAXQUEUESIZE 100
 
 void usage();
 void make_socket_reusable(int listen_sfd);
 void socket_bind(int listen_sfd, char *port);
 void socket_listen(int listen_sfd);
+int init_server(char *port);
+int accept_client(int listen_sfd);
 
 int main(int argc, char *argv[])
 {
   int listen_sfd;
   int new_sfd = -1;
+  struct pollfd fds[200];
+  int nfds;
+  int timeout;
+  int status;
 
   if (argc < 3 || argc > 4)
   {
     usage();
     return 0;
   }
-  listen_sfd = socket(AF_INET, SOCK_STREAM, 0);// use | SOCK_NONBLOCK
-  make_socket_reusable(listen_sfd);
-  socket_bind(listen_sfd, argv[1]);
-  socket_listen(listen_sfd);
-  new_sfd = accept(listen_sfd, NULL, NULL);
-  if (new_sfd == -1)
+  listen_sfd = init_server(argv[1]);
+
+  nfds = 1;
+  fds[0].fd = listen_sfd;
+  fds[0].events = POLLIN;
+  timeout = -1;
+
+  status = poll(fds, nfds, timeout);
+  if (status == -1)
   {
-    perror("Accept() failed");
+    perror("poll() failed");
+    //remember to close all FDs
     close(listen_sfd);
     exit(-1);
   }
-  printf("A peer connected to the server\n");
-  close(new_sfd);
-  sleep(1);
-  printf("We closed the connection\n");
+  if (status == 0)
+  {
+    fprintf(stderr, "poll timedout\n");
+    close(listen_sfd);
+    exit(0);
+  }
 
+  int i;
+  int current_size;
+
+  int end_server;
+  int new_size;
+  new_size = nfds;
+  current_size = new_size;
+
+  for (i = 0; i < current_size; ++i)
+  {
+    if (fds[i].revents == 0)
+      continue;
+
+    if (!(fds[i].revents != POLLIN) && !(fds[i].revents != POLLOUT))
+    {
+      printf("Error! revents = %d\n", fds[i].revents);
+      return 1;
+    }
+
+    if (fds[i].revents == POLLIN)
+    {
+      if (fds[i].fd == listen_sfd)
+      {
+	do
+	{
+	  new_sfd = accept_client(listen_sfd);
+	  if (new_sfd == -2)
+	  {
+	    break;
+	  }
+	  if (new_sfd == -1)
+	  {
+	    end_server = 1;//change to true
+	    break;
+	  }
+	  fds[new_size].fd = new_sfd;
+	  fds[new_size].events = POLLIN;
+	  ++new_size;
+	  printf("Accepted %d connection\n", new_sfd);
+	}while (new_sfd != -1);
+      }
+      else
+      {
+	// reading clients
+      }
+    }
+    else if (fds[i].revents == POLLOUT)
+    {
+      printf("HEY YOU");
+    }
+  }
+  //printf("A peer connected to the server\n");
+  //close(new_sfd);
+  //sleep(1);
+  //printf("We closed the connection\n");
   close(listen_sfd);
   return 0;
+}
+
+int accept_client(int listen_sfd)
+{
+  int new_sfd;
+  new_sfd = accept(listen_sfd, NULL, NULL);
+  if (new_sfd == -1)
+  {
+    if (errno != EWOULDBLOCK || errno != EAGAIN)
+    {
+      perror("Accept() failed");
+      close(listen_sfd);
+      exit(-1);// instead of using exit you should end the sever
+      // closing all sockets and memory
+      return -1; // is new_sfd == -1 end server
+    }
+    return -2; // normal
+  }
+  return new_sfd;
+}
+
+int init_server(char *port)
+{
+  int listen_sfd;
+
+  listen_sfd = socket(AF_INET, SOCK_STREAM, 0);// use | SOCK_NONBLOCK
+  make_socket_reusable(listen_sfd);
+  socket_bind(listen_sfd, port);
+  socket_listen(listen_sfd);
+
+  return listen_sfd;
 }
 
 /*!
